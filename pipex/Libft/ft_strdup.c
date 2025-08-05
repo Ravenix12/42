43b79231f508cc3f5dde0cefbaa67b5d6654d2e1
Ravenix12 <shivani_mariappan@mymail.sutd.edu.sg>
2025-08-05 19:54:34 +0800

    Add files via upload
    Pipex - no bonus

---
+++ pipex/Libft/ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smariapp <smariapp@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/10 17:08:01 by smariapp          #+#    #+#             */
+/*   Updated: 2025/05/14 21:40:31 by smariapp         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	size_t	length;
+	size_t	i;
+	char	*ptr;
+
+	length = ft_strlen(s);
+	ptr = malloc(length + 1);
+	i = 0;
+	while (i < length)
+	{
+		ptr[i] = s[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
